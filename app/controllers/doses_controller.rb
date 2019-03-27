2af69b5d3ab6f372c954c47234eda063d56bbcbe
gina chen <ginachen588@gmail.com>
2019-03-27 15:30:59 +0000

    added shared folder for navbar

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,34 +1,35 @@
 class DosesController < ApplicationController
-
-  def index
-    @cocktails = Cocktail.all
-    @doses = Dose.all
-  end
-
+  before_action :set_dose, only: [:destroy]
   def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
+    #does belongs to a cocktail
+
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
+      @review = Review.new
       render "cocktails/show"
     end
   end
 
   def destroy
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.find_by cocktail_id: params[:cocktail_id]
+    @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :cocktail
+  validates :description, presence: true
+  validates :rating, inclusion: { in: 0..5 }, presence: true
 end
